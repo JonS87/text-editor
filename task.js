f9b20a9b7a780fb3e95de48abd778ddaef19630d
Evgeniy Chausov <evgeniy@chausov.ru>
2024-03-26 13:42:31 +0300

    Done HW

---
+++ task.js
@@ -0,0 +1,15 @@
+const editor = document.getElementById('editor');
+const clearButton = document.getElementById('clear__button');
+
+if (localStorage.getItem('editor')) {
+  editor.value = localStorage.getItem('editor');
+}
+
+editor.addEventListener('keyup', () => {
+  localStorage.setItem('editor', editor.value);
+});
+
+clearButton.addEventListener('click', (e) => {
+  localStorage.removeItem('editor');
+  editor.value = '';
+});
